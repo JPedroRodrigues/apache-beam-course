eaba1eeeab242c4b788231fb397d4912321c6663
João Pedro Rodrigues Vieira <jpedro@DELL-G5>
2024-08-08 22:51:27 -0300

    finalizing course

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import apache_beam as beam
-from apache_beam.io import ReadFromText, ReadFromCsv
+from apache_beam.io import ReadFromText
+from apache_beam.io.textio import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions
 
 pipeline_options = PipelineOptions(argv = None)
@@ -80,9 +81,31 @@ def has_both_fields(lineTuple: tuple) -> bool:
     return all([values["rains"], values["dengue"]])
 
 
+def decompress_result_elements(element: tuple) -> tuple:
+    """
+    Receives a tuple in the format ('CE-2015-01', {'rains': [85.8], 'dengue': [175.0]})
+    Returns: a tuple in the format ("CE", "2015", "01", "85.8", "175.0")
+    """
+    key, data = element 
+    fu, year, month = key.split('-')
+    rain = str(data["rains"][0])
+    dengue = str(data["dengue"][0])
+
+    return (fu, year, month, rain, dengue)
+
+
+def convert_to_csv(lineTuple: tuple, delimiter: str = ';') -> str:
+    """
+    Receives a tuple in the format ("CE", 2015, 01, 85.8, 175.0)
+    Returns: a string in a csv format separated by a given delimiter "CE; 2015; 01; 85.8; 175.0"
+    """
+    
+    return f"{delimiter}".join(lineTuple)
+
+
 dengue = (
     pipeline
-    | "Dengue dataset read" >> ReadFromText("datasets/sample_casos_dengue.txt", skip_header_lines = 1)
+    | "Dengue dataset read" >> ReadFromText("datasets/casos_dengue.txt", skip_header_lines = 1)
     | "From string to list" >> beam.Map(lambda line: line.split("|"))
     | "From list to dictionary" >> beam.Map(lambda element: dict(zip(dengue_columns, element)))
     | "Convert date" >> beam.Map(convert_dengue_date)
@@ -96,7 +119,7 @@ dengue = (
 
 rain = (
     pipeline
-    | "Rain dataset read" >> ReadFromText("datasets/sample_chuvas.csv", skip_header_lines = 1)
+    | "Rain dataset read" >> ReadFromText("datasets/chuvas.csv", skip_header_lines = 1)
     | "Tranform rain data string to list" >> beam.Map(lambda line: line.split(","))
     | "Create FU-YYYY-MM key in a tuple" >> beam.Map(create_fu_year_month_rain_key)
     | "Sum rain occurences by the key" >> beam.CombinePerKey(sum)
@@ -112,7 +135,11 @@ result = (
     ({"rains": rain, "dengue": dengue})
     | "Group pcols" >> beam.CoGroupByKey()
     | "Filter empty values" >> beam.Filter(has_both_fields)
-    | "Show the union result" >> beam.Map(print)
+    | "Decompress result elements" >> beam.Map(decompress_result_elements)
+    | "Convert result line into csv" >> beam.Map(convert_to_csv, delimiter = ',')
+    # | "Show the union result" >> beam.Map(print)
 )
+
+result | "Create csv file" >> WriteToText("result", file_name_suffix = ".csv", header = "UF,ANO,MÊS,CHUVA,DENGUE")
 
 pipeline.run()
